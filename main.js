d04440a82a2c396ccad8a4109ad9c10554c312f2
Pradeep Bhatta <75995879+PradeepBhatta@users.noreply.github.com>
2024-10-24 14:24:24 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+document.addEventListener('scroll', () => {
+    document.querySelectorAll('.fade-in').forEach(el => {
+        const rect = el.getBoundingClientRect();
+        if (rect.top < window.innerHeight) {
+            el.classList.add('is-visible');
+        }
+    });
+});
 
