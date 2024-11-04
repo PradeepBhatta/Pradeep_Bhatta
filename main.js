4c5e2e2a06698eebf6843ee9cc441dd080d83351
Pradeep Bhatta <75995879+PradeepBhatta@users.noreply.github.com>
2024-11-04 23:36:58 +0545

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('scroll', () => {
-    document.querySelectorAll('.fade-in').forEach(el => {
-        const rect = el.getBoundingClientRect();
-        if (rect.top < window.innerHeight) {
-            el.classList.add('is-visible');
-        }
-    });
-});
-
